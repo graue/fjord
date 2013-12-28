34b813c68c9c1c3744c5dae9c02d20bba02d85b9
Scott Feeney <scott@oceanbase.org>
2013-12-28 02:45:25 -0500

    Use environ for database config details

--- project.clj
+++ project.clj
@@ -4,12 +4,16 @@
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
                  [hiccup "1.0.4"]
+                 [environ "0.4.0"]
                  [org.postgresql/postgresql "9.3-1100-jdbc41"]
                  ; Note: java.jdbc dependency must stay in sync with Lobos.
                  [org.clojure/java.jdbc "0.3.0-beta1"]
                  [lobos "1.0.0-beta1"]]
-  :plugins [[lein-ring "0.8.8"]]
+  :plugins [[lein-ring "0.8.8"]
+            [lein-environ "0.4.0"]]
   :ring {:handler fjord.app/handler}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]
-                        [ring/ring-devel "1.2.1"]]}})
+                        [ring/ring-devel "1.2.1"]]
+         :env {:fjord-db-user "dbuser"
+               :fjord-db-table "fjord"}}})
--- src/fjord/config.clj
+++ src/fjord/config.clj
@@ -1,8 +1,12 @@
-(ns fjord.config)
+(ns fjord.config
+  (:require [environ.core :refer [env]]))
 
 (def dbspec
   {:classname "org.postgresql.Driver"
    :subprotocol "postgresql"
-   :user "dbuser"
-   :password "foobar"
-   :subname "//localhost:5432/fjord"})
+   :user (get env :fjord-db-user "dbuser")
+   :password (get env :fjord-db-pass "")
+   :subname
+   (str "//" (get env :fjord-db-host "localhost")
+        ":" (get env :fjord-db-port 5432)
+        "/" (get env :fjord-db-table "fjord"))})
